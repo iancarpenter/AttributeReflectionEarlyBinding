b1968b20879044587198a89b478dd8a9fe653fdb
Ian Carpenter <ianhcarpenter@gmail.com>
2017-12-05 20:20:50 +0000

    Initial Version

--- AttributeReflectionEarlyBinding/Program.cs
+++ AttributeReflectionEarlyBinding/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BuildingCustomAttributes;
+
 
 namespace AttributeReflectionEarlyBinding
 {
@@ -10,6 +12,25 @@ namespace AttributeReflectionEarlyBinding
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("*** Value of VehicleDescriptionAttribute ***\n");
+            ReflectOnAttributesUsingEarlyBinding();
+            Console.ReadLine();
         }
+
+        private static void ReflectOnAttributesUsingEarlyBinding()
+        {
+            // get a type representing the Winnebago
+            Type t = typeof(Winnebago);
+
+            // get all attributes on the Winnebago
+            object[] customAtts = t.GetCustomAttributes(false);
+
+            // print the description
+            foreach(VehicleDescriptionAttribute v in customAtts)
+            {
+                Console.WriteLine($"-> {v.Description}\n");
+            }
+        }
+
     }
 }
